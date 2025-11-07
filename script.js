84f56c32393ddf2bc1387803b0498b26db366daf
Mag <166319982+magali-la@users.noreply.github.com>
2025-11-07 15:47:05 -0500

    (task 1): added shopping list array and three functions to add, remove, and display list

---
+++ script.js
@@ -0,0 +1,16 @@
+let shoppingList = [];
+
+// function to add item into the array
+function addItem(item) {
+    shoppingList.push(item);
+};
+
+// function to remove item from the array
+function removeItem() {
+    shoppingList.pop();
+}
+
+// function to show all the items in the shopping array in console
+function displayList() {
+    console.log(shoppingList);
+}
